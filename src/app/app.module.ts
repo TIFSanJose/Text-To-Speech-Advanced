de5b3beb83e3cd024e4bccd1e4d37b863f386ec7
ramon <chedecime@gmail.com>
2022-02-15 19:18:49 -0300

    prueba tts-advance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { TextToSpeechAdvanced } from '@ionic-native/text-to-speech-advanced/ngx'
+import { TextToSpeech }  from '@ionic-native/text-to-speech/ngx'
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy },
+      TextToSpeech,
+      TextToSpeechAdvanced
+    ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import  {TextToSpeech}  from '@ionic-native/text-to-speech/ngx';
+import { TextToSpeechAdvanced, TTSOptions, TTSVoice } from '@ionic-native/text-to-speech-advanced/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  Text = [];
+  texto:string = "Hola soy texto de prueba y estamos probando tts";
 
+  constructor(
+    private ttsAdv: TextToSpeechAdvanced,
+  ) {
+    this.Text = [
+      "Things are going well so far",
+      "That's the last straw",
+      "The best of both worlds",
+      "The person we were just talking about showed up!"      
+    ]
+  }
+  
+  reproducir(){
+        let ttsvoice : TTSVoice;
+        let ttsOps : TTSOptions = {
+          text : this.texto,
+          identifier: '',
+          cancel: true       
+        }
+
+
+
+        this.ttsAdv.getVoices()
+          .then(voices => {
+            if(voices.length > 0){
+              voices.forEach(v => {
+                console.log(v);
+                if(v.language.toLocaleLowerCase().indexOf('spanish') || v.language.toLocaleLowerCase().indexOf('español'))
+                 { ttsOps.identifier = v.identifier;
+                  ttsvoice = v;}
+              })
+            }else{
+              console.log(`no hay voces`);
+            }
+          })
+          .catch(err => console.error(err + `: Error en obtener voces`))
+          .finally(() => console.log(`fin de la ejecucion`))
+
+
+
+        this.ttsAdv.speak(this.texto)
+          .then(() => {
+            console.log(`success`);
+          })
+          .catch(err => console.error(err + `Error al reproducir audio`))
+          .finally(() => console.log(`se ejecuto speak`))
+
+
+        
+    }
 }
+
